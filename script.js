9f1d04eaf96304b6b8c5de3d4867528f6d0927be
Sam <sm2@soas.ac.uk>
2022-05-20 20:16:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+ctx.fillStyle = "blue";
+ctx.fillRect(50, 50, 100, 150);
